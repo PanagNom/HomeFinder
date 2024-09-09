f41b7870d9621529a2148a43ef72f98d0aed3b12
Panagiotis Nomikos <102253139+PanagNom@users.noreply.github.com>
2024-09-09 21:07:13 +0300

    Create a simple project layout. Add a Home view. Add a Home Controller. Add a Query Model.

---
+++ HomeFinder/Models/Query.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeFinder.Models
+{
+    public class Query
+    {
+        public string Geo_lat_from { get; set; } = string.Empty;
+        public string Geo_lat_to { get; set; } = string.Empty;
+        public string Geo_lng_from { get; set; } = string.Empty;
+        public string Geo_lng_to { get; set; } = string.Empty;
+
+        [Display(Name = "Minimum Price")]
+        public ushort MinPrice { get; set; }
+        [Display(Name = "Maximum Price")]
+        public ushort MaxPrice { get; set; }
+        [Display(Name = "Minimum Floor")]
+        public ushort MinLevel { get; set; }
+        [Display(Name = "Maximum Floor")]
+        public ushort MaxLevel { get; set; }
+        [Display(Name = "Minimum Square Meters")]
+        public ushort MinSize { get; set; }
+        [Display(Name = "Maximum Square Meters")]
+        public ushort MaxSize { get; set; }
+        [Display(Name = "Minimum Bedrooms")]
+        public ushort MinBedrooms { get; set; }
+        [Display(Name = "Maximum Bedrooms")]
+        public ushort MaxBedrooms { get; set; }
+        [Display(Name = "Minimum Construction Year")]
+        public ushort MinConstruction_year { get; set; }
+        [Display(Name = "Maximum Construction Year")]
+        public ushort MaxConstruction_year { get; set; }
+        [Display(Name = "Minimum Price Per Square Meter")]
+        public ushort MinPricePerUnitArea { get; set; }
+        [Display(Name = "Maximum Price Per Square Meter")]
+        public ushort MaxPricePerUnitArea { get; set; }
+        [Display(Name = "Minimum Energy Class")]
+        public string MinEnergyClass { get; set; } = string.Empty;
+        [Display(Name = "Maximum Energy Class")]
+        public string MaxEnergyClass { get; set; } = string.Empty;
+        [Display(Name = "Availability")]
+        public string[]? Availability { get; set; }
+    }
+}
